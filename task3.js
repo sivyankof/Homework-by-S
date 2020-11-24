6de9f7962f8f5f268cb92f5d9f15312bbb6d14f9
Artem Sivyankov <morfiy0393@gmail.com>
2020-11-24 17:43:53 +0300

    мелкие исправления локоничности

--- task3.js
+++ task3.js
@@ -3,14 +3,14 @@ var users = [
     {id: 2, name: "Петя", age: 19},
     {id: 3, name: "Маша", age: 18}
 ];
-users.push({id: 4, name: "Дима", age: 17});
-
-for (i in users) {
-   if (users[i].name == `Петя`){
-      console.log (users[i].name);     // выведит users по имени Петя
-   }
+users.push({id: 4, name: "Дима", age: 17})
+users.reverse();
+   for (i in users) {
+      if (users[i].name == `Петя`){
+         console.log (users[i].name);     // выведит users по имени Петя
+                                 }
 }
 
 let sums = users.reduce((sum,current) => sum + current.age, 0);  //  выводит сумму всех age = 74
 
-console.log (sums);console.log (sums);
+console.log (sums);
