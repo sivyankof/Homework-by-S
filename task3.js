97a3acb8e6c901a6af8a4dccbe485f71c966de79
Artem Sivyankov <morfiy0393@gmail.com>
2020-11-30 13:48:14 +0300

    отступы

--- task3.js
+++ task3.js
@@ -1,7 +1,7 @@
 var users = [
-  { id: 1, name: "Вася", age: 20 },
-  { id: 2, name: "Петя", age: 19 },
-  { id: 3, name: "Маша", age: 18 },
+    { id: 1, name: "Вася", age: 20 },
+    { id: 2, name: "Петя", age: 19 },
+    { id: 3, name: "Маша", age: 18 },
 ];
 users.push({ id: 4, name: "Дима", age: 17 }); //добавляем в массив еще один объект
 users.reverse();
