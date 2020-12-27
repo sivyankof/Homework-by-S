3f2be767572f7f947ddd7f815e922fb9dc1b2af9
Artem Sivyankov <morfiy0393@gmail.com>
2020-12-27 21:17:39 +0300

    интервал через тернарный оператор

--- task-15/assets/scripts/script.js
+++ task-15/assets/scripts/script.js
@@ -10,15 +10,18 @@ console.log(names);
 let namesOdd = document.querySelectorAll(`li:nth-child(odd)`);
 for (let el in namesOdd) {
     namesOdd[el].style = "color: red";
-    setInterval(() => namesOdd[el].style = namesOdd[el].style = "color: blue", 2000);
-    setInterval(() => namesOdd[el].style = namesOdd[el].style = "color: red", 4000);
+    setInterval(() => (namesOdd[el].style = "color: blue"), 2000);
+    setInterval(() => (namesOdd[el].style = "color: red"), 4000);
 }
 console.log(namesOdd);
 
 let namesEven = document.querySelectorAll(`li:nth-child(even)`);
 for (let el in namesEven) {
     namesEven[el].style = "color: blue";
-    setInterval(() => namesEven[el].style = namesEven[el].style = "color: red", 2000);
-    setInterval(() => namesEven[el].style = namesEven[el].style = "color: blue", 4000);
+    setInterval(() => {
+        namesEven[el].style.color == "blue"
+            ? (namesEven[el].style = "color: red")
+            : (namesEven[el].style = "color: blue");
+    }, 2000);
 }
 console.log(namesEven);
