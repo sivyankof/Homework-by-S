4e58f800b5d455cbc8935ebdb548026f7b6b5a3c
Artem Sivyankov <morfiy0393@gmail.com>
2021-01-28 17:10:48 +0300

    add await and catch

--- script.js
+++ script.js
@@ -11,18 +11,30 @@ function cleanRoom(dirtiLevel) {
 }
 
 async function cleanRooms(dirtiLevel_1, dirtiLevel_2, dirtiLevel_3) {
-    return Promise.all([
+   
+    let arr = Promise.all([
         cleanRoom(dirtiLevel_1),
         cleanRoom(dirtiLevel_2),
         cleanRoom(dirtiLevel_3),
-    ])
-        .then((resolve) => {
-            console.log(`Уборка проведена успешно за ${resolve[0]} секунд`);
-            console.log(`Уборка проведена успешно за ${resolve[1]} секунд`);
-            console.log(`Уборка проведена успешно за ${resolve[2]} секунд`);
+    ]);
 
-        })
-        .catch((err) => console.log(err));
+    // .then((resolve) => {
+    //     console.log(`Уборка проведена успешно за ${resolve[0]} секунд`);
+    //     console.log(`Уборка проведена успешно за ${resolve[1]} секунд`);
+    //     console.log(`Уборка проведена успешно за ${resolve[2]} секунд`);
+    // })
+    // .catch((err) => console.log(err));
+    try {
+
+         let a = await arr;
+
+         console.log(`Уборка проведена успешно за ${a[0]} секунд`);
+         console.log(`Уборка проведена успешно за ${a[1]} секунд`);
+         console.log(`Уборка проведена успешно за ${a[2]} секунд`);
+
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 cleanRooms(1, 2, 11);
